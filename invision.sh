7f182b8077cdd38d5445996b5e8f13f23cf98a93
Maik Brendler <maik.brendler@injixo.com>
2015-02-20 06:18:04 +0100

    Add installed features and simplify update.

--- invision.sh
+++ invision.sh
@@ -15,51 +15,48 @@ $SCRIPT CMD [OPTIONS]
 
   help              -- show help overview
   install [FEATURE] -- install InVision project
+  installed         -- list installed features
   update [FEATURE]  -- update InVision project
 EOF
 }
 
+function installed_features(){
+  for feature_dir in "$INVISION_WORK"/* ; do
+    if test -d "$feature_dir/.git" ; then
+      basename "$feature_dir"
+    fi
+  done
+}
+
 function update_features(){
-  max_width=0
-  for feature_dir in "$INVISION_WORK"/* ; do
-    if test -d "$feature_dir" ; then
-      current_length=$(basename "$feature_dir" | wc -c)
-      max_width=$(($current_length > $max_width ? $current_length : $max_width))
-    fi
+  local features=$(installed_features)
+  local max_length=0
+  for feature in $features ; do
+    max_length=$((${#feature} > max_length ? ${#feature} : max_length))
   done
 
-  for feature_dir in "$INVISION_WORK"/* ; do
-    if test -d "$feature_dir" ; then
-      name=$(basename "$feature_dir")
-    fi
-  done
-
-  for feature_dir in "$INVISION_WORK"/* ; do
-    if test -d "$feature_dir" ; then
-      branch=$(git -C "$feature_dir" symbolic-ref -q --short HEAD 2>/dev/null)
-      name=$(basename "$feature_dir")
-      if test $? -eq 0 ; then
-        printf "%-${max_width}s - " "$name"
-        if test "$branch" = master ; then
-          changed=$(git -C "$feature_dir" status --porcelain)
-          if test -z "$changed" ; then
-            git -C "$feature_dir" fetch > /dev/null
-            change_count=$(git -C "$feature_dir" rev-list HEAD...origin/master --count)
-            if test $change_count -ne 0 ; then
-              git -C "$feature_dir" pull > /dev/null
-              # TODO: prepare: bundle, migrate, ...
-              # TODO: restart pow
-              echo "updated ($change_count)"
-            else
-              echo "up-to-date"
-            fi
-          else
-            echo "ignored - has changes"
-          fi
+  for feature in $features ; do
+    local feature_dir="$INVISION_WORK/$feature"
+    local branch=$(git -C "$feature_dir" symbolic-ref -q --short HEAD 2>/dev/null)
+    printf "%-${max_length}s - " "$feature"
+    if test "$branch" = master ; then
+      local changed=$(git -C "$feature_dir" status --porcelain)
+      if test -z "$changed" ; then
+        git -C "$feature_dir" fetch > /dev/null
+        let change_count=$(git -C "$feature_dir" rev-list HEAD...origin/master --count)
+        if test $change_count -ne 0 ; then
+          git -C "$feature_dir" pull > /dev/null
+          # TODO: prepare: bundle, migrate, ...
+          # TODO: restart pow
+          echo "updated ($change_count)"
         else
-          echo "ignored - not on master"
+          echo "up-to-date"
         fi
+      else
+        echo "ignored - has changes"
       fi
+    else
+      echo "ignored - not on master"
     fi
   done
 }
@@ -70,6 +67,10 @@ help)
 ;;
 
 install)
+;;
+
+installed)
+  installed_features
 ;;
 
 update)
