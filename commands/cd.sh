1d47b8b9690d916e6b905c13e5e60c06c5a37b71
Maik Brendler <maik.brendler@injixo.com>
2015-02-21 18:28:28 +0100

    Make 'cd' a real command and display how to install it.

---
+++ commands/cd.sh
@@ -0,0 +1,19 @@
+
+function doc_cd() {
+  echo '  cd FEATURE        -- switch to the features directory'
+}
+
+function run_cd() {
+  cat << EOF
+To use this, add the following to your ~/.zshrc:
+
+function i {
+  if test "$1" = cd ; then
+    shift
+    cd "$(command i dir "$@")"
+  else
+    command i "$@"
+  fi
+}
+EOF
+}
