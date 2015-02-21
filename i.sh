eb8adb7cbad93f8be56f3c4ab669f363e53a3334
Maik Brendler <maik.brendler@injixo.com>
2015-02-21 19:50:26 +0100

    Use automatic command abbreviations.
    Inspired by mercurial.

--- i.sh
+++ i.sh
@@ -13,7 +13,25 @@ read_config
 
 log_clear
 
-CMD=$1
+function get_completed_command() {
+  local command_files=( "$COMMANDS_DIR/$1"* )
+  if test -z "$1" || test ${#command_files[@]} -eq 0 ; then
+    source "$COMMANDS_DIR/help.sh"
+    run_help
+    exit 1
+  elif test ${#command_files[@]} -eq 1 ; then
+    basename "${command_files[0]%.*}"
+  else
+    echo "command '$1' is ambiguous:"
+    echo -n "  "
+    for command_file in "${command_files[@]}" ; do
+      echo -n " $(basename ${command_file%.*})"
+    done
+    exit 1
+  fi
+}
+
+CMD="$(get_completed_command "$1")"
 
 if test -e "$COMMANDS_DIR/$CMD.sh" ; then
   log_message '========================================'
