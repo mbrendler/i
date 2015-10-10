9b0906e9541512f5f4883606eb1d0694a4d297c8
Maik Brendler <maik.brendler@injixo.com>
2015-10-10 14:36:35 +0200

    Simplify iwfm start, stop and reset command args
    Only restart iWFM after resetting, if it was running before.

--- commands/iwfm.sh
+++ commands/iwfm.sh
@@ -5,39 +5,20 @@ function doc_iwfm() {
 }
 
 function run_iwfm() {
-  case "$2" in
-    306*)
-      local iwfm=classic306
-      local tenant=$iwfm
-      local database=default
-      local language=${3:-31}
-      ;;
-    307*)
-      local iwfm=classic
-      local tenant=$iwfm
-      local database=default
-      local language=${3:-31}
-      ;;
-    *)
-      local iwfm=injixo
-      local tenant=${3:-2}
-      local database=$tenant
-      local language=${4:-31}
-      ;;
-  esac
+  local name=$2
   log pushd "$PROJECTS_DIR/iwfm-wine"
   case "$1" in
     # reset
     r*)
-      run_iwfm_reset "$iwfm" "$database" "$tenant" "$language"
+      run_iwfm_reset "$name"
       ;;
     # start
     star*)
-      run_iwfm_start "$tenant"
+      run_iwfm_start "$name"
       ;;
     # stop
     sto*)
-      run_iwfm_stop "$tenant"
+      run_iwfm_stop "$name"
       ;;
     # status
     stat*)
@@ -55,29 +36,30 @@ function run_iwfm() {
 }
 
 function run_iwfm_reset() {
-  local iwfm=$1
-  local database=$2
-  local tenant=$3
-  local language=$4
-  run_iwfm_stop "$tenant"
-  rake "server:$iwfm:db:reset[$database,$language]"
-  # TODO: only start if was running
-  run_iwfm_start "$tenant"
+  local name=$1
+  local type; type=$(iwfm_type_by_name "$name")
+  local database=$name
+  local iwfm_was_running=0
+  log iwfm_is_running iescon "$name" && iwfm_was_running=1
+  test ! "$type" = injixo && database=default
+  test $iwfm_was_running -eq 1 && run_iwfm_stop "$name"
+  rake "server:$type:db:reset[$database,31]"
+  test $iwfm_was_running -eq 1 && run_iwfm_start "$name"
 }
 
 function run_iwfm_start() {
-  local tenant=$1
-  local running_script="$HOME/.wine/drive_c/iwfmcommon/running.sh"
-  # TODO: use iwfm_is_running function
-  until bash "$running_script" iescon "$tenant" > /dev/null ; do
-    rake "server:$iwfm:start:ies[$tenant]"
+  local name=$1
+  local type; type=$(iwfm_type_by_name "$name")
+  until log iwfm_is_running iescon "$name" > /dev/null ; do
+    rake "server:$type:start:ies[$name]"
     sleep 0.2
   done
 }
 
 function run_iwfm_stop() {
-  local tenant=$1
-  rake "server:$iwfm:stop[$tenant]"
+  local name=$1
+  local type; type=$(iwfm_type_by_name "$name")
+  rake "server:$type:stop[$name]"
 }
 
 function iwfm_all_injixo_tenants() {
