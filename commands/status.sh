e4e6a87a6dd9b220836d16ce77d2d94c11568181
Maik Brendler <maik.brendler@injixo.com>
2015-10-23 21:15:36 +0200

    Alias 'iwfm status' command to 'status'

---
+++ commands/status.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+function doc_status() {
+  echo '  status   -- show status information'
+}
+
+function run_status() {
+  i iwfm status
+  # TODO: add nginx
+  # TODO: add forman
+  # TODO: add project servers
+}
