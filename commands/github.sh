5cc3dcf2c6f3be45d99125e178a408e9c3f9c3d2
Maik Brendler <maik.brendler@injixo.com>
2015-12-02 19:36:02 +0100

    Add open-github-issue with github command

--- commands/github.sh
+++ commands/github.sh
@@ -1,12 +1,24 @@
 
 function doc_github() {
-  echo "  github [FEATURE]  -- open browser with feature on github"
+  echo "  github [FEATURE] [ISSUE] -- open browser with feature on github"
 }
 
 function run_github() {
-  local feature="$(get_feature_name github "$1")"
+  local feature
+  local issue
+  if [[ "${1-}" =~ ^[0-9]+$ ]] ; then
+    feature="$(get_feature_name github "")"
+    issue="$1"
+  else
+    feature="$(get_feature_name github "${1-}")"
+    issue="${2-}"
+  fi
   if test -n "$feature" ; then
-    open "$I_GITHUB_BASE_URL/$feature"
+    if test -n "$issue" ; then
+      open "$I_GITHUB_BASE_URL/$feature/issues/$issue"
+    else
+      open "$I_GITHUB_BASE_URL/$feature"
+    fi
     sleep 0.1
     log osascript "$HERE/go_to_window.scpt"
   fi
