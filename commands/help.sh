a119872904bd1addedb3f114b087b90eb93cda41
Maik Brendler <maik.brendler@injixo.com>
2016-02-19 21:21:06 +0100

    Hide commands starting with '_' in help

--- commands/help.sh
+++ commands/help.sh
@@ -4,9 +4,15 @@ function doc_help() {
 }
 
 function run_help() {
+  local all=${1-}
   echo "$0 CMD [OPTIONS]"
   echo
-  for command_script in $HERE/commands/*.sh ; do
+  if test "$all" = '--all' -o "$all" = '-a' ; then
+    local match="$HERE/commands/*.sh"
+  else
+    local match="$HERE/commands/[^_]*.sh"
+  fi
+  for command_script in $match ; do
     source "$command_script"
     "doc_$(basename "${command_script%.*}")"
   done
