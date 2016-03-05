f32b255106df53ba569f1840732caba2978845cc
Maik Brendler <maik.brendler@injixo.com>
2016-03-05 22:34:19 +0100

    Extend zsh-completion tests

--- test_zsh_completion.zsh
+++ test_zsh_completion.zsh
@@ -26,10 +26,20 @@ assert() {
   local content="$(cat)"
   for x in $@ ; do
     if ! grep " $x " <(echo $content) > /dev/null ; then
-      echo bad $x not in \'$content\'
+      echo bad \'$x\' not in \'$content\'
+      echo expected: \'$@\'
       exit 1
     fi
   done
+  echo ok
+}
+
+assert-empty() {
+  local content="$(cat)"
+  if [ -n "$content" ] ; then
+    echo bad \'$content\' should be empty
+    exit 1
+  fi
   echo ok
 }
 
@@ -38,12 +48,17 @@ is-extra-command-installed() {
 }
 
 run_completion 2 i | assert cd github help iwfm
+run_completion 2 i '' | assert cd github help iwfm
 run_completion 2 i c | assert cd github help iwfm
 run_completion 2 i c hallo | assert cd github help iwfm
 run_completion 3 i cd '' | assert Me iwfm-api iwfm-ruby iwfm-test iwfm
+run_completion 2 i i | assert cd github help iwfm
 run_completion 3 i iwfm '' | assert reset start stop status help
+run_completion 3 i status '' | assert-empty
+run_completion 3 i st '' | assert-empty
 
 if is-extra-command-installed build ; then
+  run_completion 2 i b | assert build cd github help iwfm
   run_completion 4 i build fail '' | assert frankenstein injixo opti xlink
   run_completion 5 i build fail frank '' | assert ies.log security.log
 fi
@@ -56,4 +71,5 @@ if is-extra-command-installed crawl ; then
   # TODO: run_completion 4 i crawl -u '' | assert-empty
   run_completion 5 i crawl -u hehe '' | assert -s -l -r -c -u -p session-token tenants iwfm-versions query run
   run_completion 4 i crawl run '' | assert ---files---
+  run_completion 6 i crawl -u hehe run '' | assert ---files---
 fi
