13a39fa243705db67a76935fa823c09b15f7f349
Maik Brendler <maik.brendler@injixo.com>
2016-03-08 19:23:16 +0100

    Remove useless environment initialisation

--- commands/_list-features.sh
+++ commands/_list-features.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-INSTALLED_HERE="$(python -c "import os; print(os.path.dirname(os.path.realpath('$BASH_SOURCE')))")"
-source "$INSTALLED_HERE/../lib.sh"
-
-read_config
-
 function doc__list-features() {
   echo "  _list-features           -- list installed features"
 }
