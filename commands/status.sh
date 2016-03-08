c6e2ace5fb7279a47a228beb5082403b2a6ba440
Maik Brendler <maik.brendler@injixo.com>
2016-03-08 23:16:39 +0100

    Extend status with processes and features

--- commands/status.sh
+++ commands/status.sh
@@ -6,7 +6,43 @@ function doc--status() {
 
 function run--status() {
   i iwfm status
-  # TODO: add nginx
-  # TODO: add forman
-  # TODO: add project servers
+
+  echo
+  (status-simple nginx ;
+   status-simple postgres ;
+   status-simple redis) | \
+     status-add-heading process,status | \
+     status-pretty '"%10s - %6s\n", $1, $2'
+
+  echo
+  status-feature-ports | \
+    status-feature-insert-status | \
+    status-add-heading feature,status,port| \
+    status-pretty '"%18s - %6s %5s\n", $1, $2, $3'
+}
+
+function status-pretty() {
+  awk -F, "{ printf($1) }"
+}
+
+function status-add-heading() {
+  echo "$@"
+  cat
+}
+
+function status-simple() {
+  echo "$1,$(updown pgrep "$1")"
+}
+
+function status-feature-insert-status() {
+  local netstat_anv;netstat_anv="$(netstat -anv)"
+  while read -r x ; do
+    local name;name="$(echo "$x" | cut -d, -f1)"
+    local port;port="$(echo "$x" | cut -d, -f2)"
+    echo "$name,$(echo "$netstat_anv" | updown grep "\.$port .*LISTEN"),$port"
+  done
+}
+
+function status-feature-ports() {
+  sed -nE 's/^([^:]+):.*-p ([0-9]+).*$/\1,\2/p' "$PROJECTS_DIR/grabbel/Procfile"
 }
