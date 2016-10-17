4323841fe19fbc65e4236d17d4ba9174973b7f94
Maik Brendler <maik.brendler@injixo.com>
2016-10-17 16:20:57 +0200

    Add pomodoro command

---
+++ commands/pomodoro.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+I_POMODORO_FILE="/tmp/pomodoro"
+I_POMODORO_DEFAULT_MINUTES=25
+
+function doc--pomodoro() {
+  echo 'pomodoro [up|halt|status] -- start pomodoro'
+}
+
+function run--pomodoro() {
+  case "${1-"$I_POMODORO_DEFAULT_MINUTES"}" in
+    h*) pomodoro-stop ;;
+    u*) pomodoro-start "${2-$I_POMODORO_DEFAULT_MINUTES}";;
+    *) pomodoro-status ;;
+  esac
+}
+
+function pomodoro-start() {
+  local minutes="$1"
+  if ! pomodoro-is-running ; then
+    nohup bash -c "echo \$\$ ; echo $minutes ; sleep $((minutes * 60)) ; n complete pomodoro" > "$I_POMODORO_FILE" 2> "$I_POMODORO_FILE.err" &
+  else
+    echo pomodoro is already running
+    pomodoro-status
+    exit 1
+  fi
+}
+
+function pomodoro-stop() {
+  kill "$(pomodoro-previous-pid)"
+}
+
+function pomodoro-status() {
+  if pomodoro-is-running ; then
+    local seconds;seconds="$(pomodoro-current-time)"
+    local s;s=$((-1 * seconds))
+    while ((s >= 60)) ; do
+      s=$((s - 60))
+    done
+    echo pomodoro time: "$((-1 * seconds / 60))min ${s}s"
+  else
+    echo pomodoro is not running
+  fi
+}
+
+function pomodoro-previous-pid() {
+  head -n1 "$I_POMODORO_FILE" 2>/dev/null || true
+}
+
+function pomodoro-previous-minutes() {
+  tail -n1 "$I_POMODORO_FILE" 2>/dev/null || true
+}
+
+function pomodoro-current-time() {
+  local last_pomodoro_started;last_pomodoro_started="$(stat -c %Y "$I_POMODORO_FILE")"
+  local current_time;current_time="$(date +%s)"
+  echo $((current_time - last_pomodoro_started - 60 * $(pomodoro-previous-minutes)))
+}
+
+function pomodoro-is-running() {
+  local pid;pid="$(pomodoro-previous-pid)"
+  if [ -n "$pid" ] && ps -p "$pid" > /dev/null ; then
+    if [ "$(pomodoro-current-time)" -le 0 ] ; then
+      return 0
+    fi
+  fi
+  return 1
+}
