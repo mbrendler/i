60cab25039e7520b6fb29accb32d2d9a6e9ed83d
Maik Brendler <maik.brendler@injixo.com>
2017-01-26 12:57:22 +0100

    Add possibility to open PRs to github command

--- commands/github.sh
+++ commands/github.sh
@@ -3,9 +3,25 @@ function doc--github() {
   echo 'github [FEATURE] [ISSUE] -- open browser with feature on github'
 }
 
+function doc--github-options() {
+  cat <<EOF
+-p -- open pull-request with current branch
+EOF
+}
+
 function run--github() {
   local feature
   local issue
+  local branch
+  local url="$I_GITHUB_BASE_URL"
+  if [ "${1-}" = '-p' ] ; then
+    branch="$(github-current-branch)"
+    if [ "$branch" = master ] ; then
+      >&2 echo can not create pull request with master branch
+      exit 1
+    fi
+    shift
+  fi
   if [[ "${1-}" =~ ^[0-9]+$ ]] ; then
     feature="$(get_feature_name github "")"
     issue="$1"
@@ -14,8 +30,13 @@ function run--github() {
     issue="${2-}"
   fi
   if test -n "$feature" ; then
-    local url="$I_GITHUB_BASE_URL/$feature"
+    url="$I_GITHUB_BASE_URL/$feature"
     test -n "$issue" && url="$url/issues/$issue"
-    i-browser "$url"
+    test -n "$branch" && url="${url}/compare/${branch}?expand=1"
   fi
+  i-browser "$url"
+}
+
+function github-current-branch() {
+  git rev-parse --abbrev-ref HEAD
 }
