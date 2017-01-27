c3b0f5d89dd83539e657273c150aa72f20bda013
Maik Brendler <maik.brendler@injixo.com>
2017-01-27 05:53:52 +0100

    Simplify dir command

--- commands/dir.sh
+++ commands/dir.sh
@@ -4,16 +4,10 @@ function doc--dir() {
 }
 
 function run--dir() {
-  if test -n "${1-}" ; then
+  local dir="$PROJECTS_DIR"
+  if [ -n "${1-}" ] ; then
     local feature;feature="$(get_feature_name dir "$1")"
-    local feature_dir="$PROJECTS_DIR/$feature"
-    if test -e "$feature_dir"; then
-      echo "$feature_dir"
-    else
-      >&2 echo "Not installed feature '$feature'"
-      exit 1
-    fi
-  else
-    echo "$PROJECTS_DIR"
+    dir="$dir/$feature"
   fi
+  echo "$dir"
 }
