bca08fdc3db80d8c2e0a9ee8c2430131a71f9d9a
Maik Brendler <maik.brendler@injixo.com>
2017-01-27 05:59:46 +0100

    Simplify edit command

--- commands/edit.sh
+++ commands/edit.sh
@@ -4,16 +4,7 @@ function doc--edit() {
 }
 
 function run--edit() {
-  local feature ; feature="$(get_feature_name edit "${1-}")"
-  local feature_dir="$PROJECTS_DIR/$feature"
-  if test -n "$feature" ; then
-    if test -e "$feature_dir"; then
-      log pushd "$feature_dir"
-      test -n "${1-}" && shift
-      exec "$I_EDITOR" "$@"
-    else
-      >&2 echo "Not installed feature '$feature'"
-      exit 1
-    fi
-  fi
+  local feature;feature="$(get_feature_name edit "${1-}")"
+  log pushd "$PROJECTS_DIR/$feature"
+  exec "$I_EDITOR"
 }
